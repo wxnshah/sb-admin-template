9de0796c0165d6e163d7901cecb16f98e00fdc54
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2024-05-08 09:07:09 +0800

    Added Select2 and Datatable

---
+++ js/my-scripts.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    $( '.select2' ).select2( {
+        theme: "bootstrap-5",
+        width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
+        placeholder: $( this ).data( 'placeholder' ),
+    } );
+});
