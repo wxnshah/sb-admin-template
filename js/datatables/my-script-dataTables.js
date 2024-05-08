3b01a5242b4795c0d1fd2831e428ed1fea1d7c66
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2024-05-08 09:14:40 +0800

    Update my-script-dataTables.js

--- js/datatables/my-script-dataTables.js
+++ js/datatables/my-script-dataTables.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     var table = $('#example').DataTable( {
         // dom: "<'row'<'col-md-6 mb-1'B><'col-md-6 mb-1'f>>t<'row'<'col-md-1 mt-2'l><'col-md-2'i><'col-md-9'p>>",
-        dom: "<'row'<'col-md-6 mb-1'B><'col-md-6 mb-1'f>>t<'row'<'col-md-2'li><'col-md-10'p>>",
-        // dom: '<"top"i>rt<"bottom"flp><"clear">',
+        // dom: "<'row'<'col-md-6 mb-1'B><'col-md-6 mb-1'f>>t<'row'<'col-md-2'li><'col-md-10'p>>",
+        dom: "<'row'<'col-sm-12 col-md-6'><'col-sm-12 col-md-6'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'li><'col-sm-12 col-md-7'p>>",
         buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
     } );            
     table.buttons().container()
