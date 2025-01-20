08627d7f608dd2b8d072f8a09c000bf7bad20ac8
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2025-01-20 12:04:49 +0800

    Update my-script-dataTables.js | Update DataTables

--- js/datatables/my-script-dataTables.js
+++ js/datatables/my-script-dataTables.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    var table = $('#example').DataTable( {
+    var table = $('.datatable').DataTable( {
         // dom: "<'row'<'col-md-6 mb-1'B><'col-md-6 mb-1'f>>t<'row'<'col-md-1 mt-2'l><'col-md-2'i><'col-md-9'p>>",
         // dom: "<'row'<'col-md-6 mb-1'B><'col-md-6 mb-1'f>>t<'row'<'col-md-2'li><'col-md-10'p>>",
         dom: "<'row'<'col-sm-12 col-md-6'><'col-sm-12 col-md-6'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'li><'col-sm-12 col-md-7'p>>",
         buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
     } );            
     table.buttons().container()
-        .appendTo( '#example_wrapper .col-md-6:eq(0)' );
+        .appendTo( '.card-body .col-md-6:eq(0)' );
 } );
