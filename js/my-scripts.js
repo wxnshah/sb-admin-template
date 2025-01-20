fa4072c7b22801e6077a8721ba60819cb96ce21a
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2025-01-20 11:54:29 +0800

    Update my-scripts.js | Update DataTables

--- js/my-scripts.js
+++ js/my-scripts.js
@@ -24,3 +24,10 @@ $(document).ready(function() {
         $(this).css('height', height);
     });
 });
+
+$(document).ready(function() {
+    $('.datatable').DataTable({
+        lengthChange: false,
+        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+    }).buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
+});
